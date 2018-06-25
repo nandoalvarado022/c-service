ac2d9be6b3194a92ac451417ff0f1adf55b37abd
Nando Alvarado <nandoalvarado022@gmail.com>
2018-06-24 19:54:59 -0500

    ajuste

--- conex.php
+++ conex.php
@@ -2,8 +2,8 @@
 	
 	session_start();
 	include "functions.php";
-	// $conexion = mysqli_connect('www.clsolutions.com.co', 'id2431471_proyec', 'id2431471_proyec', 'id2431471_recursos');
-	$conexion = mysqli_connect('localhost', 'root', '', 'id2431471_recursos');
+	$conexion = mysqli_connect('www.clsolutions.com.co', 'id2431471_proyec', 'id2431471_proyec', 'id2431471_recursos');
+	// $conexion = mysqli_connect('localhost', 'root', '', 'id2431471_recursos');
 	mysqli_set_charset($conexion, "utf8");
 	if (!$conexion) {
 	    // die('No pudo conectarse: ' . mysql_error());
